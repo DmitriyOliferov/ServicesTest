b72a2fac8533e0ed2e324f9a4a4cce5e60a6eef9
Dmitriy Oliferov <DrEfrit@yandex.ru>
2022-04-01 17:12:47 +0300

    add service, foreground service and intent service

--- app/src/main/java/com/oliferov/servicestest/MainActivity.kt
+++ app/src/main/java/com/oliferov/servicestest/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.oliferov.servicestest
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.oliferov.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.buttonService.setOnClickListener {
+            stopService(MyForegroundService.newIntent(this))
+            startService(MyService.newIntent(this,20))
+        }
+        binding.buttonForegroundService.setOnClickListener {
+            ContextCompat.startForegroundService(
+                this,
+                MyForegroundService.newIntent(this)
+            )
+        }
+        binding.buttonIntentService.setOnClickListener {
+            ContextCompat.startForegroundService(
+                this,
+                MyIntentService.newIntent(this)
+            )
+        }
     }
 }
