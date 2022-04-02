05b1d185e9fe7b4df32bdfa952484ba90b5a44fc
Dmitriy Oliferov <DrEfrit@yandex.ru>
2022-04-02 23:17:20 +0300

    add job service

--- app/src/main/java/com/oliferov/servicestest/MainActivity.kt
+++ app/src/main/java/com/oliferov/servicestest/MainActivity.kt
@@ -3,6 +3,10 @@ package com.oliferov.servicestest
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
+import android.content.ComponentName
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,12 +20,14 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private var page = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.buttonService.setOnClickListener {
             stopService(MyForegroundService.newIntent(this))
-            startService(MyService.newIntent(this,20))
+            startService(MyService.newIntent(this, 20))
         }
         binding.buttonForegroundService.setOnClickListener {
             ContextCompat.startForegroundService(
@@ -34,6 +40,25 @@ class MainActivity : AppCompatActivity() {
                 this,
                 MyIntentService.newIntent(this)
             )
+        }
+        binding.buttonJobScheduler.setOnClickListener {
+            val componentName = ComponentName(this, MyJobService::class.java)
+
+            val jobInfo = JobInfo.Builder(MyJobService.ID_SERVICE, componentName)
+                .setRequiresCharging(true)
+//                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .build()
+
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val intent = MyJobService.newIntent(page++)
+                jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            } else {
+                startService(MyIntentService2.newIntent(this,page++))
+            }
+
         }
     }
 }
---
+++ app/src/main/java/com/oliferov/servicestest/MyJobIntentService.kt
@@ -0,0 +1,4 @@
+package com.oliferov.servicestest
+
+class MyJobIntentService {
+}
